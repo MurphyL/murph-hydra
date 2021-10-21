7e444c0feb16229a797087a0a4edbced8bd59616
MurphyL <murphyl@outlook.com>
2021-10-21 13:40:02 +0800

    update

--- hydra/features/hydra-etl/src/main/java/com/murphyl/hydra/etl/EtlFacade.java
+++ hydra/features/hydra-etl/src/main/java/com/murphyl/hydra/etl/EtlFacade.java
@@ -6,7 +6,7 @@ import com.murphyl.x.MurphFeature;
  * ETL - 门面
  *
  * @date: 2021/10/20 20:14
- * @author: hao.luo <hao.luo@china.zhaogang.com>
+ * @author: murph
  */
 public class EtlFacade implements MurphFeature {
 }
