a3b1409387a255dd03205af8e9f841c5aef543cd
MurphyL <murphyl@outlook.com>
2021-10-22 11:30:29 +0800

    合并

--- hydra/features/hydra-task/src/main/java/com/murphyl/hydra/task/TaskFacade.java
+++ hydra/features/hydra-task/src/main/java/com/murphyl/hydra/task/TaskFacade.java
@@ -1,5 +1,6 @@
 package com.murphyl.hydra.task;
 
+import com.murphyl.hydra.task.etl.EtlTask;
 import com.murphyl.x.Feature;
 import com.murphyl.x.PlugIn;
 
@@ -9,6 +10,6 @@ import com.murphyl.x.PlugIn;
  * @date: 2021/10/20 20:14
  * @author: murph
  */
-@PlugIn({})
+@PlugIn({EtlTask.class})
 public class TaskFacade implements Feature {
 }
--- hydra/features/hydra-task/src/main/java/com/murphyl/hydra/task/etl/EtlTask.java
+++ hydra/features/hydra-task/src/main/java/com/murphyl/hydra/task/etl/EtlTask.java
@@ -1,10 +1,10 @@
 package com.murphyl.hydra.task.etl;
 
 /**
- * -
+ * ETL - 任务
  *
  * @date: 2021/10/21 14:26
- * @author: hao.luo <hao.luo@china.zhaogang.com>
+ * @author: murph
  */
 public class EtlTask {
 }
