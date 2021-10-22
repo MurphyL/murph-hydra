22e865b1fcf189efe9bb1fb6bae8d362d16af29f
murph <murphyl@outlook.com>
2021-10-22 23:23:49 +0800

    配置中心

---
+++ plug-ins/hydra-config/src/main/java/com/murphyl/hydra/feature/config/ConfigFeature.java
@@ -0,0 +1,17 @@
+package com.murphyl.hydra.feature.config;
+
+import com.murphyl.hydra.facade.Feature;
+
+/**
+ * 配置 - 模块
+ * author: murph
+ * 2021/10/22 - 23:14
+ */
+public class ConfigFeature implements Feature {
+
+    @Override
+    public void execute() {
+
+    }
+
+}
